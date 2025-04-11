02c3985be94033936ce3ffc7d0a3b8aa58be5e00
Ned Batchelder <ned@nedbatchelder.com>
2025-04-11 10:56:37 -0400

    adapt to older vims

---
+++ run_fix_vim.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# If vim doesn't look in .config/ then symlink to get our stuff.
+if ! vim --version | grep -q XDG_CONFIG; then
+    ln -sf ~/.config/vim/vimrc ~/.vimrc
+    ln -sf ~/.config/vim ~/.vim
+fi
+vim --not-a-term +PlugInstall +qall
