4ab3e72b13b63b409f296b9f80d75f893fc9a337
Ned Batchelder <ned@nedbatchelder.com>
2025-04-16 07:27:45 -0400

    vim: also move .viminfo to XDG

--- run_fix_vim.sh
+++ run_fix_vim.sh
@@ -3,16 +3,22 @@
 # If we had vim state in the home directory, move it to the XDG locations.
 if [ -d ~/.backup ]; then
     mkdir -p $XDG_STATE_HOME/vim
-    mv ~/.backup $XDG_STATE_HOME/vim/backup
+    mv -v ~/.backup $XDG_STATE_HOME/vim/backup
 fi
 
 if [ -d ~/.vimundo ]; then
     mkdir -p $XDG_STATE_HOME/vim
-    mv ~/.vimundo $XDG_STATE_HOME/vim/undo
+    mv -v ~/.vimundo $XDG_STATE_HOME/vim/undo
+fi
+
+if [ -f ~/.viminfo ]; then
+    mkdir -p $XDG_STATE_HOME/vim
+    mv -v ~/.viminfo $XDG_STATE_HOME/vim/viminfo
 fi
 
 # If vim doesn't look in .config/ then symlink to get our stuff.
 if ! vim --version | grep -q XDG_CONFIG_HOME; then
+    echo linking vim config from home
     ln -sf $XDG_CONFIG_HOME/vim/vimrc ~/.vimrc
     ln -sf $XDG_CONFIG_HOME/vim ~/.vim
 fi
