0486b502485882c954d550eef09c2d673f431a49
Ned Batchelder <ned@nedbatchelder.com>
2025-04-16 07:15:35 -0400

    vim: use XDG locations

--- run_fix_vim.sh
+++ run_fix_vim.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
-# If vim doesn't look in .config/ then symlink to get our stuff.
-if ! vim --version | grep -q XDG_CONFIG; then
-    ln -sf ~/.config/vim/vimrc ~/.vimrc
-    ln -sf ~/.config/vim ~/.vim
+# If we had vim state in the home directory, move it to the XDG locations.
+if [ -d ~/.backup ]; then
+    mkdir -p $XDG_STATE_HOME/vim
+    mv ~/.backup $XDG_STATE_HOME/vim/backup
 fi
+
+if [ -d ~/.vimundo ]; then
+    mkdir -p $XDG_STATE_HOME/vim
+    mv ~/.vimundo $XDG_STATE_HOME/vim/undo
+fi
+
+# If vim doesn't look in .config/ then symlink to get our stuff.
+if ! vim --version | grep -q XDG_CONFIG_HOME; then
+    ln -sf $XDG_CONFIG_HOME/vim/vimrc ~/.vimrc
+    ln -sf $XDG_CONFIG_HOME/vim ~/.vim
+fi
+
+# Install and/or update all the plugins.
 vim --not-a-term +PlugInstall +PlugUpdate +qall
