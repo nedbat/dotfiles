c787997a737185c3eb2c7209fd135ef947406356
Ned Batchelder <ned@nedbatchelder.com>
2025-04-16 20:02:22 -0400

    if xdg/shell_history exists, don't clobber it with ~/.history

--- run_fix_history.sh
+++ run_fix_history.sh
@@ -2,5 +2,9 @@
 
 # If we had shell history in the home directory, move it to the XDG locations.
 if [ -f ~/.history ]; then
-    mv -v ~/.history $XDG_STATE_HOME/shell_history
+    if [ ! -f $XDG_STATE_HOME/shell_history ]; then
+        mv -v ~/.history $XDG_STATE_HOME/shell_history
+    else
+        rm ~/.history
+    fi
 fi
