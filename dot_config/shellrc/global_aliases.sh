547194c4e74cb3927927886d90b20c30e1de01e1
Ned Batchelder <ned@nedbatchelder.com>
2025-07-02 11:29:20 -0400

    ,1 and ,11 global aliases

--- dot_config/shellrc/global_aliases.sh
+++ dot_config/shellrc/global_aliases.sh
@@ -2,4 +2,6 @@ if [[ $SHELL_TYPE == zsh ]]; then
     alias -g ,G='| egrep '
     alias -g ,L='| wc -l'
     alias -g ,Q='2>/dev/null'
+    alias -g ,11='| head -n $(($(tput lines)-4))'
+    alias -g ,1='--color=always ,11'
 fi
