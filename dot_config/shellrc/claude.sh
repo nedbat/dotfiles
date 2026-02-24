e3211eefe04fdc9e8e9a5c300e4ad609cf58dbc8
Ned Batchelder <ned@nedbatchelder.com>
2026-02-24 07:51:35 -0500

    claude shouldn't suggest the next prompt

--- dot_config/shellrc/claude.sh
+++ dot_config/shellrc/claude.sh
@@ -1,1 +1,4 @@
+# Don't change the title of my terminal tab, because it doesn't change back when I exit claude.
 export CLAUDE_CODE_DISABLE_TERMINAL_TITLE=1
+# Don't suggest the next prompt.
+export CLAUDE_CODE_ENABLE_PROMPT_SUGGESTION=false
