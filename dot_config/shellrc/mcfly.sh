e8bfd6b3f461dec8c0fdb5a7c27cf53080f5dd4d
Ned Batchelder <ned@nedbatchelder.com>
2026-03-10 16:24:05 -0400

    atuin in mcfly

--- dot_config/shellrc/mcfly.sh
+++ dot_config/shellrc/mcfly.sh
@@ -1,8 +1,14 @@
 # https://github.com/cantino/mcfly
 if [[ -n $PS1 ]]; then
-    if command -v mcfly >/dev/null; then
-        export MCFLY_RESULTS=30
-        export MCFLY_HISTORY_LIMIT=50000
-        eval "$(mcfly init $SHELL_TYPE)"
-    fi
+    #if command -v mcfly >/dev/null; then
+    #    export MCFLY_RESULTS=30
+    #    export MCFLY_HISTORY_LIMIT=50000
+    #    eval "$(mcfly init $SHELL_TYPE)"
+    #fi
+
+    eval "$(atuin init $SHELL_TYPE)"
+    # https://til.simonwillison.net/macos/atuin
+    autoload -U history-search-end
+    zle -N history-beginning-search-backward-end history-search-end
+    bindkey "^[[A" history-beginning-search-backward-end
 fi
