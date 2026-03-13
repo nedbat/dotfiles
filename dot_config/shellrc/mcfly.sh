2c1c88dd647d7ecfff64097b9d1c96fd5fab3189
Ned Batchelder <ned@nedbatchelder.com>
2026-03-13 08:54:47 -0400

    atuin on linux

--- dot_config/shellrc/mcfly.sh
+++ dot_config/shellrc/mcfly.sh
@@ -8,7 +8,14 @@ if [[ -n $PS1 ]]; then
 
     eval "$(atuin init $SHELL_TYPE)"
     # https://til.simonwillison.net/macos/atuin
+    # updated to work on Linux over ssh
     autoload -U history-search-end
     zle -N history-beginning-search-backward-end history-search-end
+    zle -N history-beginning-search-forward-end history-search-end
+    # Works on Linux:
+    bindkey "${terminfo[kcuu1]}" history-beginning-search-backward-end
+    bindkey "${terminfo[kcud1]}" history-beginning-search-forward-end
+    # Works on Mac:
     bindkey "^[[A" history-beginning-search-backward-end
+    bindkey "^[[B" history-beginning-search-forward-end
 fi
