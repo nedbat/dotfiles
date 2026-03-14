bb53bd358466722ed6ca91e20976701237a9df07
Ned Batchelder <ned@nedbatchelder.com>
2026-03-14 14:41:40 -0400

    mcfly.sh -> history.sh

--- dot_config/shellrc/history.sh
+++ dot_config/shellrc/history.sh
@@ -1,5 +1,5 @@
-# https://github.com/cantino/mcfly
 if [[ -n $PS1 ]]; then
+    # https://github.com/cantino/mcfly
     #if command -v mcfly >/dev/null; then
     #    export MCFLY_RESULTS=30
     #    export MCFLY_HISTORY_LIMIT=50000
