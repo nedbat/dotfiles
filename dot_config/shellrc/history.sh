de232ebda546150a4481bf881c932c4ae4c77bf9
Ned Batchelder <ned@nedbatchelder.com>
2026-03-14 16:29:21 -0400

    don't atuin if no atuin

--- dot_config/shellrc/history.sh
+++ dot_config/shellrc/history.sh
@@ -6,16 +6,18 @@ if [[ -n $PS1 ]]; then
     #    eval "$(mcfly init $SHELL_TYPE)"
     #fi
 
-    eval "$(atuin init $SHELL_TYPE)"
-    # https://til.simonwillison.net/macos/atuin
-    # updated to work on Linux over ssh
-    autoload -U history-search-end
-    zle -N history-beginning-search-backward-end history-search-end
-    zle -N history-beginning-search-forward-end history-search-end
-    # Works on Linux:
-    bindkey "${terminfo[kcuu1]}" history-beginning-search-backward-end
-    bindkey "${terminfo[kcud1]}" history-beginning-search-forward-end
-    # Works on Mac:
-    bindkey "^[[A" history-beginning-search-backward-end
-    bindkey "^[[B" history-beginning-search-forward-end
+    if command -v atuin >/dev/null; then
+        eval "$(atuin init $SHELL_TYPE)"
+        # https://til.simonwillison.net/macos/atuin
+        # updated to work on Linux over ssh
+        autoload -U history-search-end
+        zle -N history-beginning-search-backward-end history-search-end
+        zle -N history-beginning-search-forward-end history-search-end
+        # Works on Linux:
+        bindkey "${terminfo[kcuu1]}" history-beginning-search-backward-end
+        bindkey "${terminfo[kcud1]}" history-beginning-search-forward-end
+        # Works on Mac:
+        bindkey "^[[A" history-beginning-search-backward-end
+        bindkey "^[[B" history-beginning-search-forward-end
+    fi
 fi
