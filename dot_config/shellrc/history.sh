328f557d9b0a0423821156d79daedbd8b25b2787
Ned Batchelder <ned@nedbatchelder.com>
2026-03-20 12:28:35 -0400

    history binding is different for different shells

--- dot_config/shellrc/history.sh
+++ dot_config/shellrc/history.sh
@@ -1,23 +1,21 @@
 if [[ -n $PS1 ]]; then
-    # https://github.com/cantino/mcfly
-    #if command -v mcfly >/dev/null; then
-    #    export MCFLY_RESULTS=30
-    #    export MCFLY_HISTORY_LIMIT=50000
-    #    eval "$(mcfly init $SHELL_TYPE)"
-    #fi
-
     if command -v atuin >/dev/null; then
         eval "$(atuin init $SHELL_TYPE)"
-        # https://til.simonwillison.net/macos/atuin
-        # updated to work on Linux over ssh
-        autoload -U history-search-end
-        zle -N history-beginning-search-backward-end history-search-end
-        zle -N history-beginning-search-forward-end history-search-end
-        # Works on Linux:
-        bindkey "${terminfo[kcuu1]}" history-beginning-search-backward-end
-        bindkey "${terminfo[kcud1]}" history-beginning-search-forward-end
-        # Works on Mac:
-        bindkey "^[[A" history-beginning-search-backward-end
-        bindkey "^[[B" history-beginning-search-forward-end
+        if [[ $SHELL_TYPE == zsh ]]; then
+            # https://til.simonwillison.net/macos/atuin
+            # updated to work on Linux over ssh
+            autoload -U history-search-end
+            zle -N history-beginning-search-backward-end history-search-end
+            zle -N history-beginning-search-forward-end history-search-end
+            # Works on Linux:
+            bindkey "${terminfo[kcuu1]}" history-beginning-search-backward-end
+            bindkey "${terminfo[kcud1]}" history-beginning-search-forward-end
+            # Works on Mac:
+            bindkey "^[[A" history-beginning-search-backward-end
+            bindkey "^[[B" history-beginning-search-forward-end
+        fi
+        if [[ $SHELL_TYPE == bash ]]; then
+            bind '"\e[A": history-search-backward'
+        fi
     fi
 fi
