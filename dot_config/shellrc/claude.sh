4397dcda742c7075582d7611d9ef947d01003c3d
Ned Batchelder <ned@nedbatchelder.com>
2026-04-16 15:23:32 -0400

    claude

--- dot_config/shellrc/claude.sh
+++ dot_config/shellrc/claude.sh
@@ -1,4 +1,8 @@
 # Don't change the title of my terminal tab, because it doesn't change back when I exit claude.
 export CLAUDE_CODE_DISABLE_TERMINAL_TITLE=1
+
 # Don't suggest the next prompt.
 export CLAUDE_CODE_ENABLE_PROMPT_SUGGESTION=false
+
+# https://karanbansal.in/blog/claude-code-lsp/
+export ENABLE_LSP_TOOL=1
